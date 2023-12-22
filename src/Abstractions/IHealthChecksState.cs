3338aef1cf71b6c522adb112f020d1316548dca5
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-22 22:50:46 +0100

    Add HealthCheckReport

---
+++ src/Abstractions/IHealthChecksState.cs
@@ -0,0 +1,21 @@
+﻿using Models;
+
+namespace Abstractions;
+
+/// <summary>
+/// Contract for healthchecks state.
+/// </summary>
+public interface IHealthChecksState
+{
+    /// <summary>
+    /// Updates healthcheck timestamp for the resource.
+    /// </summary>
+    /// <param name="resourceName"></param>
+    public void Update(ResourceName resourceName);
+
+    /// <summary>
+    /// Builds aggregated healthcheck report about all resources.
+    /// </summary>
+    /// <returns></returns>
+    public HealthCheckReport BuildReport();
+}
