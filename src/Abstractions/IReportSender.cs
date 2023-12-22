46485e27f17bff34d96a522291b27684e2999b78
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-22 23:32:49 +0100

    Add IReportSender

---
+++ src/Abstractions/IReportSender.cs
@@ -0,0 +1,16 @@
+﻿using Models;
+
+namespace Abstractions;
+
+/// <summary>
+/// Healthcheks report sender.
+/// </summary>
+public interface IReportSender
+{
+    /// <summary>
+    /// Sends healthcheck report.
+    /// </summary>
+    /// <param name="report">Healthcheks report.</param>
+    /// <param name="ct">Token for cancel the operation.</param>
+    public Task SendAsync(HealthCheckReport report, CancellationToken ct);
+}
