f19279d6ea72cdf9e25b664d0c172094e0df39bc
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-22 20:24:18 +0100

    Add resource name type

---
+++ src/Models/ResourceName.cs
@@ -0,0 +1,27 @@
+﻿namespace Models;
+
+/// <summary>
+/// Name for a resource.
+/// </summary>
+public sealed class ResourceName
+{
+    /// <summary>
+    /// Name.
+    /// </summary>
+    public string Value { get; }
+
+    /// <summary>
+    /// Create resource name.
+    /// </summary>
+    /// <param name="name">Name.</param>
+    /// <exception cref="ArgumentNullException">Throws if name is null or empty</exception>
+    public ResourceName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentNullException(nameof(name), "Name cannot be null or empty.");
+        }
+
+        Value = name;
+    }
+}
