317e524f216d5675f8f5bb8571490d2b2034acd7
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-23 18:20:56 +0100

    Add JSON attributes for DTO

--- src/Transport.Models/HealthCheckReport.cs
+++ src/Transport.Models/HealthCheckReport.cs
@@ -1,17 +1,22 @@
-﻿namespace Transport.Models;
+﻿using Newtonsoft.Json;
+
+namespace Transport.Models;
 
 /// <summary>
 /// Aggregated healthcheck report DTO.
 /// </summary>
+[JsonObject(MemberSerialization.OptIn)]
 public sealed class HealthCheckReport
 {
     /// <summary>
     /// Report creation time.
     /// </summary>
+    [JsonProperty("timestamp", Required = Required.Always)]
     public required DateTimeOffset Created { get; init; }
 
     /// <summary>
     /// Resources in the report.
     /// </summary>
+    [JsonProperty("resources", Required = Required.Always)]
     public required IEnumerable<HealthCheckReportItem> ReportItems { get; init; }
 }
--- src/Transport.Models/HealthCheckReportItem.cs
+++ src/Transport.Models/HealthCheckReportItem.cs
@@ -1,19 +1,24 @@
 ﻿using Models;
 
+using Newtonsoft.Json;
+
 namespace Transport.Models;
 
 /// <summary>
 /// Report item about resource healthcheck DTO.
 /// </summary>
+[JsonObject(MemberSerialization.OptIn)]
 public sealed class HealthCheckReportItem
 {
     /// <summary>
     /// Resournce name.
     /// </summary>
+    [JsonProperty("resource_name", Required = Required.Always)]
     public required string ResourceName { get; init; }
 
     /// <summary>
     /// Resource status.
     /// </summary>
+    [JsonProperty("resource_status", Required = Required.Always)]
     public required ResourceStatus Status { get; init; }
 }
