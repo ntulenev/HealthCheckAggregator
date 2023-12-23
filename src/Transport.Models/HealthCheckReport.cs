3209caf18505b3612b18d84f9084ee12cb3badd6
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-23 18:04:32 +0100

    Add HC Report DTO for export

---
+++ src/Transport.Models/HealthCheckReport.cs
@@ -0,0 +1,17 @@
+﻿namespace Transport.Models;
+
+/// <summary>
+/// Aggregated healthcheck report DTO.
+/// </summary>
+public sealed class HealthCheckReport
+{
+    /// <summary>
+    /// Report creation time.
+    /// </summary>
+    public required DateTimeOffset Created { get; init; }
+
+    /// <summary>
+    /// Resources in the report.
+    /// </summary>
+    public required IEnumerable<HealthCheckReportItem> ReportItems { get; init; }
+}
---
+++ src/Transport.Models/HealthCheckReportItem.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+
+namespace Transport.Models;
+
+/// <summary>
+/// Report item about resource healthcheck DTO.
+/// </summary>
+public sealed class HealthCheckReportItem
+{
+    /// <summary>
+    /// Resournce name.
+    /// </summary>
+    public required string ResourceName { get; init; }
+
+    /// <summary>
+    /// Resource status.
+    /// </summary>
+    public required ResourceStatus Status { get; init; }
+}
