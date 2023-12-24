52bc90bcc963129a83cae546d44b18721e2506e7
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-24 18:24:51 +0100

    Add RawSender

---
+++ src/Abstractions/IRawSender.cs
@@ -0,0 +1,15 @@
+﻿namespace Abstractions;
+
+/// <summary>
+/// Send data to the destination.
+/// </summary>
+/// <typeparam name="TData"></typeparam>
+public interface IRawSender<TData>
+{
+    /// <summary>
+    /// Sends data.
+    /// </summary>
+    /// <param name="data">Data to send.</param>
+    /// <param name="ct">Token for cancel the operation.</param>
+    public Task SendAsync(TData data, CancellationToken ct);
+}
