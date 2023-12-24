ed13745cc2965ea2beac7052e444ac87542dd1ef
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-24 19:13:56 +0100

    Add ReportSender Configuration

---
+++ src/Transport/Configuration/ReportSenderConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace Transport.Configuration;
+
+/// <summary>
+/// Report sender configuration.
+/// </summary>
+public sealed class ReportSenderConfiguration
+{
+    /// <summary>
+    /// Report sender destination url.
+    /// </summary>
+    public required string Url { get; init; }
+}
