b6451c7fb43d20f7ccdf5713b3fccd6afd5ca362
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-24 23:48:46 +0100

    Add BackgroundServices

---
+++ src/Service/Services/ResourceObserverService.cs
@@ -0,0 +1,28 @@
+﻿using Abstractions;
+
+namespace Service.Services
+{
+    /// <summary>
+    /// Service for observing resources health checks.
+    /// </summary>
+    public sealed class ResourceObserverService : BackgroundService
+    {
+        /// <summary>
+        /// Creates <see cref="ResourceObserverService"/>.
+        /// </summary>
+        /// <param name="resourceObserver">Resources observer.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Throws exception is <paramref name="resourceObserver"/> is null.</exception>
+        public ResourceObserverService(IResourcesObserver resourceObserver)
+        {
+            ArgumentNullException.ThrowIfNull(resourceObserver);
+            _resourcesObserver = resourceObserver;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken) =>
+                                        await _resourcesObserver.ObserveAsync(stoppingToken)
+                                                                .ConfigureAwait(false);
+
+        private readonly IResourcesObserver _resourcesObserver;
+    }
+}
---
+++ src/Service/Services/SenderService.cs
@@ -0,0 +1,28 @@
+﻿using Abstractions;
+
+namespace Service.Services;
+
+/// <summary>
+/// Service that sends healthcheck reports.
+/// </summary>
+public sealed class SenderService : BackgroundService
+{
+    /// <summary>
+    /// Creates <see cref="SenderService"/>.
+    /// </summary>
+    /// <param name="reportProcessor">Healthchecks report processor.</param>
+    /// <exception cref="ArgumentNullException">
+    /// Throws if <paramref name="reportProcessor"/> is null.
+    /// </exception>
+    public SenderService(IReportProcessor reportProcessor)
+    {
+        ArgumentNullException.ThrowIfNull(reportProcessor);
+        _reportProcessor = reportProcessor;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+                                => await _reportProcessor.ProcessAsync(stoppingToken)
+                                                         .ConfigureAwait(false);
+
+    private readonly IReportProcessor _reportProcessor;
+}
