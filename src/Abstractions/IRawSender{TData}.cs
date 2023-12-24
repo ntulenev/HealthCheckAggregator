df62c3aac16bc8f8990d1f4a1f199eb5d2397faf
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-24 18:56:05 +0100

    Add HttpJsonSender

--- src/Abstractions/IRawSender{TData}.cs
+++ src/Abstractions/IRawSender{TData}.cs
@@ -10,6 +10,7 @@ public interface IRawSender<TData>
     /// Sends data.
     /// </summary>
     /// <param name="data">Data to send.</param>
+    /// <param name="uri">Destination uri.</param>
     /// <param name="ct">Token for cancel the operation.</param>
-    public Task SendAsync(TData data, CancellationToken ct);
+    public Task SendAsync(TData data, Uri uri, CancellationToken ct);
 }
---
+++ src/Transport/HttpJsonSender.cs
@@ -0,0 +1,42 @@
+﻿using Abstractions;
+
+using System.Text;
+
+namespace Transport;
+
+public sealed class HttpJsonSender : IRawSender<string>
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="HttpJsonSender"/>.
+    /// </summary>
+    /// <param name="clientProxy">Htto client wrapper.</param>
+    /// <exception cref="ArgumentNullException">
+    /// Throws if clientProxy is null.</exception>
+    public HttpJsonSender(IHttpClientProxy clientProxy)
+    {
+        ArgumentNullException.ThrowIfNull(clientProxy);
+        _clientProxy = clientProxy;
+    }
+
+    /// <inheritdoc/>
+    /// <exception cref="ArgumentNullException">
+    /// Throws is data for uri is null.</exception>
+    /// <exception cref="OperationCanceledException">
+    /// Throws if token is expired.</exception>
+    public async Task SendAsync(string data, Uri uri, CancellationToken ct)
+    {
+        ArgumentNullException.ThrowIfNull(data);
+        ct.ThrowIfCancellationRequested();
+
+        using var content =
+           new StringContent(
+           data,
+           Encoding.UTF8,
+           CONTENT_TYPE);
+
+        await _clientProxy.Client.PostAsync(uri, content, ct);
+    }
+
+    private const string CONTENT_TYPE = "application/json";
+    private readonly IHttpClientProxy _clientProxy;
+}
