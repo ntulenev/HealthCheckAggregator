a54499b00986d652cd22adff9ac67dadc16fab48
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-24 23:21:30 +0100

    Add IResourceCheckerProcessor

---
+++ src/Abstractions/IResourceCheckerProcessor.cs
@@ -0,0 +1,13 @@
+﻿namespace Abstractions;
+
+/// <summary>
+/// Process resources healthchecks.
+/// </summary>
+public interface IResourceCheckerProcessor
+{
+    /// <summary>
+    /// Rusn process resources healthchecks.
+    /// </summary>
+    /// <param name="ct">Token for cancel.</param>
+    public Task ProcessAsync(CancellationToken ct);
+}
