f82d6f4d34d8c082ad8895159a3a8f19f59afa51
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-24 20:31:13 +0100

    IResourceChecker

---
+++ src/Abstractions/IResourceChecker.cs
@@ -0,0 +1,13 @@
+﻿namespace Abstractions;
+
+/// <summary>
+/// Checks the resource healthcheck.
+/// </summary>
+public interface IResourceChecker
+{
+    /// <summary>
+    /// Checks the resource healthcheck.
+    /// </summary>
+    /// <param name="ct">Token for cancel.</param>
+    public Task CheckAsync(CancellationToken ct);
+}
