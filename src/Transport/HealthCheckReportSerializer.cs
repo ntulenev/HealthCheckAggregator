845df7e59db2b9588065cdd2e728d8b5d76771a5
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-24 08:16:35 +0100

    Add HealthCheckReportSerializer

---
+++ src/Transport/HealthCheckReportSerializer.cs
@@ -0,0 +1,36 @@
+﻿using Abstractions;
+using Transport.Models;
+
+using Newtonsoft.Json;
+
+using System.Globalization;
+using System.Text;
+
+namespace Transport;
+
+/// <summary>
+/// Json serializer for <see cref="HealthCheckReport"/>.
+/// </summary>
+public sealed class HealthCheckReportSerializer :
+    ISerializer<HealthCheckReport, string>
+{
+    ///<inheritdoc/>
+    ///<exception cref="ArgumentNullException">
+    ///Throws if <paramref name="source"/> is null.
+    ///</exception>
+    public string Serialize(HealthCheckReport source)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        var sb = new StringBuilder();
+        var sw = new StringWriter(sb, CultureInfo.InvariantCulture);
+        using (var jsonWriter = new JsonTextWriter(sw))
+        {
+            _serializer.Serialize(jsonWriter, source);
+        }
+
+        return sw.ToString();
+    }
+
+    private static readonly JsonSerializer _serializer = JsonSerializer.CreateDefault();
+}
