e732bea8efe9f7b77547cc6c1299a53a6dc5ff93
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-24 08:06:56 +0100

    Add ISerializer abstraction

---
+++ src/Abstractions/ISerializer{TSource, TTarget}.cs
@@ -0,0 +1,16 @@
+﻿namespace Abstractions;
+
+/// <summary>
+/// Provides contract of serialization from <typeparamref name="TSource"/> to <typeparamref name="TTarget"/>.
+/// </summary>
+/// <typeparam name="TSource">Type that should be serialized</typeparam>
+/// <typeparam name="TTarget">Target type of the serialization.</typeparam>
+public interface ISerializer<TSource, TTarget>
+{
+    /// <summary>
+    /// Serialize from <typeparamref name="TSource"/> to <typeparamref name="TTarget"/>
+    /// </summary>
+    /// <param name="source">Object that should be serialized.</param>
+    /// <returns>Serialized result.</returns>
+    public TTarget Serialize(TSource source);
+}
