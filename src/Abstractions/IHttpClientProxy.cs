0d21117c0e37e085385777f8fc9cbd852a772e85
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-24 18:55:46 +0100

    Add IHttpClientProxy

---
+++ src/Abstractions/IHttpClientProxy.cs
@@ -0,0 +1,13 @@
+﻿namespace Abstractions;
+
+/// <summary>
+/// Wrapper around <see cref="System.Net.Http.HttpClient"/>,
+/// allowing the configuration of an HTTP client for a specific use case.
+/// </summary>
+public interface IHttpClientProxy
+{
+    /// <summary>
+    /// Property containing a configured <see cref="HttpClient"/>.
+    /// </summary>
+    public HttpClient Client { get; }
+}
