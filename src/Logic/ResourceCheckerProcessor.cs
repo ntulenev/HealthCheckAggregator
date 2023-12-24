14eea5e7294084492e0c63b9f32736cbb07b5fc2
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-24 23:21:41 +0100

    Add ResourceCheckerProcessor

---
+++ src/Logic/ResourceCheckerProcessor.cs
@@ -0,0 +1,61 @@
+﻿using Abstractions;
+using Models;
+
+using Microsoft.Extensions.Logging;
+
+namespace Logic;
+
+/// <summary>
+/// Checks the resource healthcheck.
+/// </summary>
+public sealed class ResourceCheckerProcessor : IResourceCheckerProcessor
+{
+    /// <summary>
+    /// Creates new instance of <see cref="ResourceCheckerProcessor"/>.
+    /// </summary>
+    /// <param name="logger">Logger.</param>
+    /// <param name="resourceChecker">Resource checker.</param>
+    /// <param name="resourceHealthCheck">Target resource.</param>
+    /// <exception cref="ArgumentNullException">
+    /// Throws if any argument is null.
+    /// </exception>
+    public ResourceCheckerProcessor(ILogger<ResourceCheckerProcessor> logger,
+                                    IResourceChecker resourceChecker,
+                                    ResourceHealthCheck resourceHealthCheck)
+    {
+        ArgumentNullException.ThrowIfNull(logger);
+        ArgumentNullException.ThrowIfNull(resourceChecker);
+        ArgumentNullException.ThrowIfNull(resourceHealthCheck);
+
+        _logger = logger;
+        _resourceChecker = resourceChecker;
+        _resourceHealthCheck = resourceHealthCheck;
+    }
+
+    /// <inheritdoc/>
+    /// <exception cref="OperationCanceledException">
+    /// Throw if token is expired.
+    /// </exception>
+    public async Task ProcessAsync(CancellationToken ct)
+    {
+        while (true)
+        {
+            ct.ThrowIfCancellationRequested();
+
+            try
+            {
+                await _resourceChecker.CheckAsync(_resourceHealthCheck, ct);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex, "Error while checking resource {resource}.", _resourceHealthCheck.ResourceName);
+            }
+
+            await Task.Delay(_resourceHealthCheck.RequestSettings.CheckInterval, ct);
+        }
+    }
+
+    private readonly ILogger _logger;
+    private readonly IResourceChecker _resourceChecker;
+    private readonly ResourceHealthCheck _resourceHealthCheck;
+}
