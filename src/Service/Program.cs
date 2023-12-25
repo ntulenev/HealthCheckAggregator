124381323dabf64147ab458c0c352fc437445054
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-25 09:35:54 +0100

    Add scoped dependencies

--- src/Service/Program.cs
+++ src/Service/Program.cs
@@ -9,6 +9,7 @@ using Logic;
 using Microsoft.Extensions.Options;
 
 using Serilog;
+using Abstractions.Logic;
 
 var builder = WebApplication.CreateBuilder(args);
 _ = builder.Services.AddHealthChecks();
@@ -18,6 +19,9 @@ _ = builder.Services.AddSingleton<IHealthChecksState, HealthChecksState>();
 _ = builder.Services.AddSingleton<ISerializer<Transport.Models.HealthCheckReport, string>, HealthCheckReportSerializer>();
 _ = builder.Services.AddSingleton<IReportSender, ReportSender>();
 _ = builder.Services.AddSingleton<IRawSender<string>, HttpJsonSender>();
+_ = builder.Services.AddScoped<IRawResourceChecker, HttpRawResourceChecker>();
+_ = builder.Services.AddScoped<IResourceChecker, ResourceChecker>();
+_ = builder.Services.AddScoped<IResourceCheckerProcessor, ResourceCheckerProcessor>();
 _ = builder.Services.Configure<ReportSenderConfiguration>(builder.Configuration.GetSection(nameof(ReportSenderConfiguration)));
 _ = builder.Services.AddSingleton<IValidateOptions<ReportSenderConfiguration>, ReportSenderConfigurationValidator>();
 _ = builder.Services.Configure<ReportProcessorConfiguration>(builder.Configuration.GetSection(nameof(ReportProcessorConfiguration)));
@@ -26,10 +30,7 @@ _ = builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
     loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
 
 //TODO IHttpClientProxy
-//IRawResourceChecker
-//IHealthChecksState
-//IResourceCheckerProcessor
-//IResoueceChecker
+//State configuration
 //Factory methods
 
 var app = builder.Build();
