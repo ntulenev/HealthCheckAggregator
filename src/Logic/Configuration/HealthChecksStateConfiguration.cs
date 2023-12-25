015f5e57236aa1d54eff7b91b177705d5db543a8
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-25 12:21:13 +0100

    Add xmldoc for HealthChecksStateConfiguration

--- src/Logic/Configuration/HealthChecksStateConfiguration.cs
+++ src/Logic/Configuration/HealthChecksStateConfiguration.cs
@@ -1,6 +1,12 @@
 ﻿namespace Logic.Configuration;
 
+/// <summary>
+/// Configuration for <see cref="HealthChecksState"/>.
+/// </summary>
 public sealed class HealthChecksStateConfiguration
 {
+    /// <summary>
+    /// Resources configurations.
+    /// </summary>
     public required IEnumerable<ResourceConfiguration> Resources { get; init; }
 }
