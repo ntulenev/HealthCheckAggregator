a42610a011619116b5f826b614fdd97771db0039
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-25 08:55:50 +0100

    Add ResourcesObserver

--- src/Logic/ResourcesObserver.cs
+++ src/Logic/ResourcesObserver.cs
@@ -1,11 +1,52 @@
 ﻿using Abstractions.Logic;
+using Abstractions.State;
+using Models;
+
+using Microsoft.Extensions.Logging;
 
 namespace Logic;
 
+/// <summary>
+/// Observes resources healthchecks.
+/// </summary>
 public sealed class ResourcesObserver : IResourcesObserver
 {
-    public Task ObserveAsync(CancellationToken ct)
+    /// <summary>
+    /// Creates new instance of <see cref="ResourcesObserver"/>.
+    /// </summary>
+    /// <param name="logger">Logger.</param>
+    /// <param name="state">Resources internal state.</param>
+    /// <param name="processorFactory">Factory method to create resource processor.</param>
+    public ResourcesObserver(ILogger<ResourcesObserver> logger,
+                             IHealthChecksState state,
+                             Func<ResourceHealthCheck, IResourceCheckerProcessor> processorFactory)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(logger);
+        ArgumentNullException.ThrowIfNull(state);
+        ArgumentNullException.ThrowIfNull(processorFactory);
+
+        _logger = logger;
+        _state = state;
+        _processorFactory = processorFactory;
     }
+
+    /// <inheritdoc/>
+    /// <exception cref="OperationCanceledException">
+    /// Throws if <paramref name="ct"/> is expired.
+    /// </exception>
+    public async Task ObserveAsync(CancellationToken ct)
+    {
+        _logger.LogDebug("Starting resources observer.");
+
+        ct.ThrowIfCancellationRequested();
+
+        await Task.WhenAll(_state.HealthChecks
+                                 .Select(_processorFactory)
+                                 .Select(x => x.ProcessAsync(ct)))
+                                 .ConfigureAwait(false);
+    }
+
+    private readonly IHealthChecksState _state;
+    private readonly ILogger _logger;
+    private readonly Func<ResourceHealthCheck, IResourceCheckerProcessor> _processorFactory;
 }
