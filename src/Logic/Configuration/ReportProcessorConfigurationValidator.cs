33e8af06fbab18c5fb34e2f4dcdad3c10810fe6a
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-25 08:35:42 +0100

    Add IValidateOptions validators

---
+++ src/Logic/Configuration/ReportProcessorConfigurationValidator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Options;
+using System.Diagnostics;
+
+namespace Logic.Configuration;
+
+/// <summary>
+/// Validator for <see cref="ReportProcessorConfiguration"/>.
+/// </summary>
+public sealed class ReportProcessorConfigurationValidator : IValidateOptions<ReportProcessorConfiguration>
+{
+    /// <summary>
+    /// Validates <see cref="ReportProcessorConfiguration"/>.
+    /// </summary>
+    public ValidateOptionsResult Validate(string? name, ReportProcessorConfiguration options)
+    {
+        Debug.Assert(options != null);
+
+        if (options.SendInterval <= TimeSpan.Zero)
+        {
+            return ValidateOptionsResult.Fail("Send interval must be positive.");
+        }
+
+        return ValidateOptionsResult.Success;
+    }
+}
---
+++ src/Transport/Configuration/ReportSenderConfigurationValidator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using System.Diagnostics;
+
+namespace Transport.Configuration;
+
+/// <summary>
+/// Validates <see cref="ReportSenderConfiguration"/>.
+/// </summary>
+public sealed class ReportSenderConfigurationValidator : IValidateOptions<ReportSenderConfiguration>
+{
+    /// <summary>
+    /// Validates <see cref="ReportSenderConfiguration"/>.
+    /// </summary>
+    public ValidateOptionsResult Validate(string? name, ReportSenderConfiguration options)
+    {
+        Debug.Assert(options != null);
+
+        if (string.IsNullOrEmpty(options.Url))
+        {
+            return ValidateOptionsResult.Fail("Url cannot be null or empty.");
+        }
+
+        if (!Uri.TryCreate(options.Url, UriKind.Absolute, out var uriResult) ||
+                    uriResult.Scheme != Uri.UriSchemeHttp && uriResult.Scheme != Uri.UriSchemeHttps)
+        {
+            return ValidateOptionsResult.Fail($"Url '{options.Url}' is not a valid HTTP/HTTPS URL.");
+        }
+
+        return ValidateOptionsResult.Success;
+    }
+}
