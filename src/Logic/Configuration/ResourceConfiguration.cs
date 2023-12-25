a553b9df2c6180ea769adcc151c9c8b7ba566bbe
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-25 12:21:29 +0100

    Add xmldoc for ResourceConfiguration

--- src/Logic/Configuration/ResourceConfiguration.cs
+++ src/Logic/Configuration/ResourceConfiguration.cs
@@ -1,10 +1,32 @@
 ﻿namespace Logic.Configuration;
 
+/// <summary>
+/// Resource configuration.
+/// </summary>
 public sealed class ResourceConfiguration
 {
+    /// <summary>
+    /// Resource name.
+    /// </summary>
     public required string Name { get; init; }
+
+    /// <summary>
+    /// Resource Healthcheck expiration time.
+    /// </summary>
     public required TimeSpan ExpirationPeriod { get; init; }
+
+    /// <summary>
+    /// Resource address.
+    /// </summary>
     public required Uri Uri { get; init; }
+
+    /// <summary>
+    /// Resource Healthcheck check interval.
+    /// </summary>
     public required TimeSpan CheckInterval { get; init; }
+
+    /// <summary>
+    /// Resource Healthcheck timeout.
+    /// </summary>
     public required TimeSpan Timeout { get; init; }
 }
