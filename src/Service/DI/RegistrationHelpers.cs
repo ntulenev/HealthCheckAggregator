d09ba4fea3b82e946dabc7ff64dc464a22af21bf
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-26 20:32:17 +0100

    Add DI helpers

---
+++ src/Service/DI/RegistrationHelpers.cs
@@ -0,0 +1,109 @@
+﻿using Abstractions.Logic;
+using Abstractions.State;
+using Abstractions.Transport;
+using Logic;
+using Logic.Configuration.Validation;
+using Logic.Configuration;
+using Service.Services;
+using Transport;
+using Transport.Configuration;
+using Models;
+using Transport.Mapping;
+using Transport.Configuration.Validation;
+
+using Microsoft.Extensions.Options;
+
+namespace Service.DI;
+
+/// <summary>
+/// DI Registration helpers.
+/// </summary>
+public static class RegistrationHelpers
+{
+    /// <summary>
+    /// Register hosted services.
+    /// </summary>
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        services.AddHostedService<SenderService>();
+        services.AddHostedService<ResourceObserverService>();
+    }
+
+    /// <summary>
+    /// Register resources state.
+    /// </summary>
+    public static void RegisterState(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<IHealthChecksState, HealthChecksState>();
+        services.Configure<HealthChecksStateConfiguration>(configuration.GetSection(nameof(HealthChecksStateConfiguration)));
+        services.AddSingleton<IValidateOptions<HealthChecksStateConfiguration>, HealthChecksStateConfigurationValidator>();
+        services.AddSingleton<Func<ResourceConfiguration, ResourceHealthCheck>>(conf =>
+        {
+            var name = new ResourceName(conf.Name);
+            var settings = new ResourceRequestSettings(conf.Url, conf.CheckInterval, conf.Timeout);
+            return new ResourceHealthCheck(name,
+                                           conf.ExpirationPeriod,
+                                           settings);
+        });
+    }
+
+    /// <summary>
+    /// Register resources observer logic.
+    /// </summary>
+    /// <param name="services"></param>
+    public static void RegisterObserver(this IServiceCollection services)
+    {
+        services.AddTransient<IRawResourceChecker, HttpRawResourceChecker>();
+        services.AddTransient<IResourceChecker, ResourceChecker>();
+        services.AddSingleton<IResourcesObserver, ResourcesObserver>();
+        services.AddSingleton<Func<ResourceHealthCheck, IResourceCheckerProcessor>>(sp =>
+        {
+            IResourceCheckerProcessor factory(ResourceHealthCheck res)
+            {
+                return ActivatorUtilities.CreateInstance<ResourceCheckerProcessor>(sp, res);
+            }
+            return factory;
+        });
+    }
+
+    /// <summary>
+    /// Register HttpClients.
+    /// </summary>
+    public static void RegisterHttpClient(this IServiceCollection services)
+    {
+        services.AddSingleton<IHttpClientProxy, HttpClientProxy>();
+        services.AddSingleton(sp =>
+        {
+            var config = sp.GetRequiredService<IOptions<ReportSenderConfiguration>>().Value;
+            // TODO Check If Validation Starts.
+            return new HttpClient()
+            {
+                Timeout = config.Timeout
+            };
+        });
+        services.AddSingleton<Func<TimeSpan, HttpClient>>(ts =>
+        {
+            return new HttpClient
+            {
+                Timeout = ts
+            };
+        });
+    }
+
+    /// <summary>
+    /// Register healtcheck sender logic.
+    /// </summary>
+    public static void RegisterSender(this IServiceCollection services,IConfiguration configuration)
+    {
+        services.AddSingleton<IReportProcessor, ReportProcessor>();
+        services.AddSingleton<ISerializer<Transport.Models.HealthCheckReport, string>, HealthCheckReportSerializer>();
+        services.AddSingleton<IReportSender, ReportSender>();
+        services.AddSingleton<IRawSender<string>, HttpJsonSender>();
+        services.AddAutoMapper(cfg => cfg.AddProfile<HealthCheckMappingProfile>());
+        services.Configure<ReportSenderConfiguration>(configuration.GetSection(nameof(ReportSenderConfiguration)));
+        services.AddSingleton<IValidateOptions<ReportSenderConfiguration>, ReportSenderConfigurationValidator>();
+        services.Configure<ReportProcessorConfiguration>(configuration.GetSection(nameof(ReportProcessorConfiguration)));
+        services.AddSingleton<IValidateOptions<ReportProcessorConfiguration>, ReportProcessorConfigurationValidator>();
+    }
+
+}
--- src/Service/Program.cs
+++ src/Service/Program.cs
@@ -1,75 +1,16 @@
-using Abstractions.Logic;
-using Abstractions.State;
-using Abstractions.Transport;
-using Service.Services;
-using Transport.Configuration;
-using Transport.Configuration.Validation;
-using Transport.Mapping;
-using Transport;
-using Logic.Configuration;
-using Logic.Configuration.Validation;
-using Logic;
-using Models;
-
-using Microsoft.Extensions.Options;
-
 using Serilog;
+
+using Service.DI;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHealthChecks();
-builder.Services.AddHostedService<SenderService>();
-builder.Services.AddHostedService<ResourceObserverService>();
-builder.Services.AddSingleton<IHealthChecksState, HealthChecksState>();
-builder.Services.AddSingleton<ISerializer<Transport.Models.HealthCheckReport, string>, HealthCheckReportSerializer>();
-builder.Services.AddSingleton<IReportSender, ReportSender>();
-builder.Services.AddSingleton<IHttpClientProxy, HttpClientProxy>();
-builder.Services.AddSingleton<IRawSender<string>, HttpJsonSender>();
-builder.Services.AddSingleton<IResourcesObserver, ResourcesObserver>();
-builder.Services.AddSingleton<IReportProcessor, ReportProcessor>();
-builder.Services.AddTransient<IRawResourceChecker, HttpRawResourceChecker>();
-builder.Services.AddTransient<IResourceChecker, ResourceChecker>();
-builder.Services.Configure<ReportSenderConfiguration>(builder.Configuration.GetSection(nameof(ReportSenderConfiguration)));
-builder.Services.AddSingleton<IValidateOptions<ReportSenderConfiguration>, ReportSenderConfigurationValidator>();
-builder.Services.Configure<ReportProcessorConfiguration>(builder.Configuration.GetSection(nameof(ReportProcessorConfiguration)));
-builder.Services.AddSingleton<IValidateOptions<ReportProcessorConfiguration>, ReportProcessorConfigurationValidator>();
-builder.Services.Configure<HealthChecksStateConfiguration>(builder.Configuration.GetSection(nameof(HealthChecksStateConfiguration)));
-builder.Services.AddSingleton<IValidateOptions<HealthChecksStateConfiguration>, HealthChecksStateConfigurationValidator>();
-builder.Services.AddAutoMapper(cfg => cfg.AddProfile<HealthCheckMappingProfile>());
-builder.Services.AddSingleton(sp =>
-    {
-        var config = sp.GetRequiredService<IOptions<ReportSenderConfiguration>>().Value;
-        // TODO Check If Validation Starts.
-        return new HttpClient()
-        {
-            Timeout = config.Timeout
-        };
-    });
-_ = builder.Services.AddSingleton<Func<TimeSpan, HttpClient>>(ts =>
-{
-    return new HttpClient
-    {
-        Timeout = ts
-    };
-});
-builder.Services.AddSingleton<Func<ResourceConfiguration, ResourceHealthCheck>>(conf =>
-{
-    var name = new ResourceName(conf.Name);
-    var settings = new ResourceRequestSettings(conf.Url, conf.CheckInterval, conf.Timeout);
-    return new ResourceHealthCheck(name,
-                                   conf.ExpirationPeriod,
-                                   settings);
-});
-builder.Services.AddSingleton<Func<ResourceHealthCheck, IResourceCheckerProcessor>>(sp =>
-{
-    IResourceCheckerProcessor factory(ResourceHealthCheck res)
-    {
-        return ActivatorUtilities.CreateInstance<ResourceCheckerProcessor>(sp, res);
-    }
-    return factory;
-});
+builder.Services.RegisterServices();
+builder.Services.RegisterState(builder.Configuration);
+builder.Services.RegisterObserver();
+builder.Services.RegisterHttpClient();
+builder.Services.RegisterSender(builder.Configuration);
 builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
     loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
-
 var app = builder.Build();
 app.UseHealthChecks("/hc");
 app.UseHttpsRedirection();
