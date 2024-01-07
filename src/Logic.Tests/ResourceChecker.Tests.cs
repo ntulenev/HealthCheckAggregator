9e1898f988cdddecca8fff6d0ad23c34b89a4236
Nikita Tulenev <n1tulenev@gmail.com>
2024-01-07 09:12:16 +0100

    Add ResourceChecker tests

--- src/Logic.Tests/ResourceChecker.Tests.cs
+++ src/Logic.Tests/ResourceChecker.Tests.cs
@@ -1,6 +1,7 @@
 ﻿using Abstractions.Transport;
-
 using Microsoft.Extensions.Logging;
+
+using Models;
 
 namespace Logic.Tests;
 
@@ -28,9 +29,11 @@ public class ResourceCheckerTests
     {
         // Arrange
         var logger = new Mock<ILogger<ResourceChecker>>(MockBehavior.Strict).Object;
+
         // Act
         var exception = Record.Exception(() =>
             _ = new ResourceChecker(null!, logger));
+
         // Assert
         exception.Should().BeOfType<ArgumentNullException>();
     }
@@ -41,10 +44,56 @@ public class ResourceCheckerTests
     {
         // Arrange
         var rawChecker = new Mock<IRawResourceChecker>(MockBehavior.Strict).Object;
+
         // Act
         var exception = Record.Exception(() =>
             _ = new ResourceChecker(rawChecker, null!));
+
         // Assert
         exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"{nameof(ResourceChecker)} can't check null resource")]
+    [Trait("Category", "Unit")]
+    public async Task ResourceCheckerCantCheckNullResource()
+    {
+        // Arrange
+        var rawChecker = new Mock<IRawResourceChecker>(MockBehavior.Strict).Object;
+        var logger = new Mock<ILogger<ResourceChecker>>(MockBehavior.Strict).Object;
+        var resourceChecker = new ResourceChecker(rawChecker, logger);
+        using var cts = new CancellationTokenSource();
+
+        // Act
+        var exception = await Record.ExceptionAsync(() =>
+            resourceChecker.CheckAsync(null!, cts.Token));
+
+        // Assert
+        exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"{nameof(ResourceChecker)} can't check with cancelled token")]
+    [Trait("Category", "Unit")]
+    public async Task ResourceCheckerCantCheckWithCancelledToken()
+    {
+        // Arrange
+        var rawChecker = new Mock<IRawResourceChecker>(MockBehavior.Strict).Object;
+        var logger = new Mock<ILogger<ResourceChecker>>(MockBehavior.Strict).Object;
+        var resourceChecker = new ResourceChecker(rawChecker, logger);
+        using var cts = new CancellationTokenSource();
+        cts.Cancel();
+        var targetResource = new ResourceHealthCheck(
+            new ResourceName("test"),
+            TimeSpan.FromMicroseconds(1),
+            new ResourceRequestSettings(
+                new Uri("http://example.com"),
+                TimeSpan.FromMicroseconds(1),
+                TimeSpan.FromMicroseconds(1)));
+
+        // Act
+        var exception = await Record.ExceptionAsync(() =>
+            resourceChecker.CheckAsync(targetResource, cts.Token));
+
+        // Assert
+        exception.Should().BeOfType<OperationCanceledException>();
     }
 }
