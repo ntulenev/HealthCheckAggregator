90523f07c2ad141e2ad9f2a29b89a004d65551af
Nikita Tulenev <n1tulenev@gmail.com>
2024-01-08 20:10:18 +0100

    Add ResourceCheckerProcessor tests

--- src/Logic.Tests/ResourceCheckerProcessor.Tests.cs
+++ src/Logic.Tests/ResourceCheckerProcessor.Tests.cs
@@ -14,12 +14,18 @@ public class ResourceCheckerProcessorTests
         // Arrange
         var loggerMock = new Mock<ILogger<ResourceCheckerProcessor>>();
         var resourceChecker = new Mock<IResourceChecker>(MockBehavior.Strict);
-        var resourceHealthCheck = new Mock<ResourceHealthCheck>(MockBehavior.Strict);
+        var resourceHealthCheck = new ResourceHealthCheck(
+            new ResourceName("test"),
+            TimeSpan.FromMicroseconds(1),
+            new ResourceRequestSettings(
+                new Uri("http://www.example.com"),
+                TimeSpan.FromMicroseconds(1),
+                TimeSpan.FromSeconds(1)));
 
         // Act
         var exception = Record.Exception(() =>
             _ = new ResourceCheckerProcessor(loggerMock.Object, resourceChecker.Object,
-                resourceHealthCheck.Object));
+                resourceHealthCheck));
 
         // Assert
         exception.Should().BeNull();
@@ -31,12 +37,19 @@ public class ResourceCheckerProcessorTests
     {
         // Arrange
         var resourceChecker = new Mock<IResourceChecker>(MockBehavior.Strict);
-        var resourceHealthCheck = new Mock<ResourceHealthCheck>(MockBehavior.Strict);
+        var resourceHealthCheck = new ResourceHealthCheck(
+            new ResourceName("test"),
+            TimeSpan.FromMicroseconds(1),
+            new ResourceRequestSettings(
+                new Uri("http://www.example.com"),
+                TimeSpan.FromMicroseconds(1),
+                TimeSpan.FromSeconds(1)));
 
         // Act
         var exception = Record.Exception(() =>
             _ = new ResourceCheckerProcessor(null!, resourceChecker.Object,
-                resourceHealthCheck.Object));
+                resourceHealthCheck));
+        
         // Assert
         exception.Should().BeOfType<ArgumentNullException>();
     }
@@ -47,11 +60,19 @@ public class ResourceCheckerProcessorTests
     {
         // Arrange
         var logger = new Mock<ILogger<ResourceCheckerProcessor>>();
-        var resourceHealthCheck = new Mock<ResourceHealthCheck>(MockBehavior.Strict);
+        var resourceHealthCheck = new ResourceHealthCheck(
+            new ResourceName("test"),
+            TimeSpan.FromMicroseconds(1),
+            new ResourceRequestSettings(
+                new Uri("http://www.example.com"),
+                TimeSpan.FromMicroseconds(1),
+                TimeSpan.FromSeconds(1)));
+        
         // Act
         var exception = Record.Exception(() =>
             _ = new ResourceCheckerProcessor(logger.Object, null!,
-                resourceHealthCheck.Object));
+                resourceHealthCheck));
+        
         // Assert
         exception.Should().BeOfType<ArgumentNullException>();
     }
@@ -63,11 +84,77 @@ public class ResourceCheckerProcessorTests
         // Arrange
         var logger = new Mock<ILogger<ResourceCheckerProcessor>>();
         var resourceChecker = new Mock<IResourceChecker>(MockBehavior.Strict);
+        
         // Act
         var exception = Record.Exception(() =>
             _ = new ResourceCheckerProcessor(logger.Object, resourceChecker.Object,
                 null!));
+        
         // Assert
         exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"{nameof(ResourceCheckerProcessor)} cant process on cancelled token")]
+    [Trait("Category", "Unit")]
+    public async Task ResourceCheckerProcessorCannotProcessOnCancelledToken()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ResourceCheckerProcessor>>();
+        var resourceChecker = new Mock<IResourceChecker>(MockBehavior.Strict);
+        var resourceHealthCheck = new ResourceHealthCheck(
+            new ResourceName("test"),
+            TimeSpan.FromMicroseconds(1),
+            new ResourceRequestSettings(
+                new Uri("http://www.example.com"),
+                TimeSpan.FromMicroseconds(1),
+                TimeSpan.FromSeconds(1)));
+        using var cancellationTokenSource = new CancellationTokenSource();
+        cancellationTokenSource.Cancel();
+        var processor = new ResourceCheckerProcessor(logger.Object, resourceChecker.Object,
+            resourceHealthCheck);
+
+        // Act
+        var exception =
+            await Record.ExceptionAsync(async () =>
+                await processor.ProcessAsync(cancellationTokenSource.Token));
+
+        // Assert
+        exception.Should().BeAssignableTo<OperationCanceledException>();
+    }
+
+    [Fact(DisplayName = $"{nameof(ResourceCheckerProcessor)} can process")]
+    [Trait("Category", "Unit")]
+    public async Task ResourceCheckerProcessorCanProcess()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ResourceCheckerProcessor>>();
+        var resourceChecker = new Mock<IResourceChecker>(MockBehavior.Strict);
+        var resourceHealthCheck = new ResourceHealthCheck(
+            new ResourceName("test"),
+            TimeSpan.FromMicroseconds(1),
+            new ResourceRequestSettings(
+                new Uri("http://www.example.com"),
+                TimeSpan.FromMicroseconds(1),
+                TimeSpan.FromSeconds(1)));
+        using var cancellationTokenSource = new CancellationTokenSource();
+        var checkCallCount = 0;
+        resourceChecker.Setup(x =>
+                x.CheckAsync(resourceHealthCheck, cancellationTokenSource.Token))
+            .Returns(Task.CompletedTask).Callback(() =>
+            {
+                cancellationTokenSource.Cancel();
+                checkCallCount++;
+            });
+        
+        // Act
+        var processor = new ResourceCheckerProcessor(logger.Object, resourceChecker.Object,
+            resourceHealthCheck);
+        var exception =
+            await Record.ExceptionAsync(async () =>
+                await processor.ProcessAsync(cancellationTokenSource.Token));
+        
+        // Assert
+        exception.Should().BeAssignableTo<OperationCanceledException>();
+        checkCallCount.Should().Be(1);
     }
 }
