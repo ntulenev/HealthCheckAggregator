14d53a5a3698ed1e4981591fc5e530cc265bac96
Nikita Tulenev <n1tulenev@gmail.com>
2024-01-09 17:40:08 +0100

    Add ReportSenderConfigurationValidator tests

--- src/Transport.Tests/ReportSenderConfigurationValidator.Tests.cs
+++ src/Transport.Tests/ReportSenderConfigurationValidator.Tests.cs
@@ -25,4 +25,43 @@ public class ReportSenderConfigurationValidatorTests
         // Assert
         result.Should().Be(ValidateOptionsResult.Success);
     }
+    
+    [Fact(DisplayName = $"{nameof(ReportSenderConfigurationValidator)} fails when URL is null")]
+    [Trait("Category", "Unit")]
+    public void ReportSenderConfigurationValidatorFailsWhenUrlIsNull()
+    {
+        // Arrange
+        var options = new ReportSenderConfiguration
+        {
+            Url = null!,
+            Timeout = TimeSpan.FromSeconds(10)
+        };
+        var validator = new ReportSenderConfigurationValidator();
+
+        // Act
+        var result = validator.Validate(null!, options);
+
+        // Assert
+        result.Should().NotBe(ValidateOptionsResult.Success);
+    }
+
+    [Fact(DisplayName = $"{nameof(ReportSenderConfigurationValidator)} fails when Timeout is zero")]
+    [Trait("Category", "Unit")]
+    public void ReportSenderConfigurationValidatorFailsWhenTimeoutIsZero()
+    {
+        // Arrange
+        var options = new ReportSenderConfiguration
+        {
+            Url = new Uri("https://example.com"),
+            Timeout = TimeSpan.Zero
+        };
+        var validator = new ReportSenderConfigurationValidator();
+
+        // Act
+        var result = validator.Validate(null!, options);
+
+        // Assert
+        result.Should().NotBe(ValidateOptionsResult.Success);
+    }
+    
 }
