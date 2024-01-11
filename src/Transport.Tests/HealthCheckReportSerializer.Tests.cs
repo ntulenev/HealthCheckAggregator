d258873ba4963f83eaa6dc2228409ec442da9642
Nikita Tulenev <n1tulenev@gmail.com>
2024-01-11 17:28:15 +0100

    Add HealthCheckReportSerializer tests

---
+++ src/Transport.Tests/HealthCheckReportSerializer.Tests.cs
@@ -0,0 +1,30 @@
+﻿namespace Transport.Tests;
+
+public class HealthCheckReportSerializerTests
+{
+    [Fact(DisplayName = $"{nameof(HealthCheckReportSerializer)} can be created")]
+    [Trait("Category", "Unit")]
+    public void CanBeCreated()
+    {
+        // Arrange + Act
+        var exception = Record.Exception(()
+            => _ = new HealthCheckReportSerializer());
+
+        // Assert
+        exception.Should().BeNull();
+    }
+
+    [Fact(DisplayName = $"{nameof(HealthCheckReportSerializer)} can't serialize null data")]
+    [Trait("Category", "Unit")]
+    public void CannotSerializeNullData()
+    {
+        // Arrange
+        var serializer = new HealthCheckReportSerializer();
+        
+        // Act
+        var exception = Record.Exception(() => serializer.Serialize(null!));
+        
+        // Assert
+        exception.Should().BeOfType<ArgumentNullException>();
+    }
+}
