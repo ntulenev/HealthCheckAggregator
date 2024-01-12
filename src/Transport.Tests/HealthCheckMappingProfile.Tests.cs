79a6ced96cd43875b2ad773c76822cae059a213e
Nikita Tulenev <n1tulenev@gmail.com>
2024-01-12 19:27:44 +0100

    Add HealthCheckMappingProfile tests

---
+++ src/Transport.Tests/HealthCheckMappingProfile.Tests.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+using Transport.Mapping;
+
+namespace Transport.Tests;
+
+public class HealthCheckMappingProfileTests
+{
+    [Fact(DisplayName = $"{nameof(HealthCheckMappingProfile)} is valid")]
+    [Trait("Category", "Unit")]
+    public void AutoMapperConfigurationIsValid()
+    {
+        // Arrange
+        var configuration = new MapperConfiguration(
+            cfg => { cfg.AddProfile<HealthCheckMappingProfile>(); });
+
+        // Act & Assert
+        configuration.AssertConfigurationIsValid();
+    }
+}
