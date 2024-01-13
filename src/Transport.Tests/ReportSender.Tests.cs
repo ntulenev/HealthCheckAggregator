5b86ea037dd64be9632d2479ddaa3f9f4a06f560
Nikita Tulenev <n1tulenev@gmail.com>
2024-01-13 08:31:07 +0100

    Add ReportSender tests

--- src/Transport.Tests/ReportSender.Tests.cs
+++ src/Transport.Tests/ReportSender.Tests.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 using Abstractions.Transport;
+using Models;
 using Transport.Configuration;
 
 using TModel = Transport.Models;
@@ -70,7 +71,7 @@ public class ReportSenderTests
                 serializer.Object,
                 config.Object);
         });
-        
+
         // Assert
         exception.Should().BeOfType<ArgumentNullException>();
     }
@@ -90,7 +91,7 @@ public class ReportSenderTests
             Timeout = TimeSpan.FromSeconds(10)
         };
         config.Setup(x => x.Value).Returns(configData);
-        
+
         // Act
         var exception = Record.Exception(() =>
         {
@@ -101,7 +102,7 @@ public class ReportSenderTests
                 serializer.Object,
                 config.Object);
         });
-        
+
         // Assert
         exception.Should().BeOfType<ArgumentNullException>();
     }
@@ -121,7 +122,7 @@ public class ReportSenderTests
             Timeout = TimeSpan.FromSeconds(10)
         };
         config.Setup(x => x.Value).Returns(configData);
-        
+
         // Act
         var exception = Record.Exception(() =>
         {
@@ -132,7 +133,7 @@ public class ReportSenderTests
                 serializer.Object,
                 config.Object);
         });
-        
+
         // Assert
         exception.Should().BeOfType<ArgumentNullException>();
     }
@@ -152,7 +153,7 @@ public class ReportSenderTests
             Timeout = TimeSpan.FromSeconds(10)
         };
         config.Setup(x => x.Value).Returns(configData);
-        
+
         // Act
         var exception = Record.Exception(() =>
         {
@@ -163,7 +164,7 @@ public class ReportSenderTests
                 null!,
                 config.Object);
         });
-        
+
         // Assert
         exception.Should().BeOfType<ArgumentNullException>();
     }
@@ -177,7 +178,7 @@ public class ReportSenderTests
         var mapper = new Mock<IMapper>(MockBehavior.Strict);
         var rawSender = new Mock<IRawSender<string>>(MockBehavior.Strict);
         var serializer = new Mock<ISerializer<TModel.HealthCheckReport, string>>(MockBehavior.Strict);
-        
+
         // Act
         var exception = Record.Exception(() =>
         {
@@ -188,8 +189,87 @@ public class ReportSenderTests
                 serializer.Object,
                 null!);
         });
-        
+
         // Assert
         exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"{nameof(ReportSender)} can't send null report")]
+    [Trait("Category", "Unit")]
+    public async Task CannotSendNullReport()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ReportSender>>();
+        var mapper = new Mock<IMapper>(MockBehavior.Strict);
+        var rawSender = new Mock<IRawSender<string>>(MockBehavior.Strict);
+        var serializer = new Mock<ISerializer<TModel.HealthCheckReport, string>>(MockBehavior.Strict);
+        var config = new Mock<IOptions<ReportSenderConfiguration>>(MockBehavior.Strict);
+        var configData = new ReportSenderConfiguration
+        {
+            Url = new Uri("http://example.com"),
+            Timeout = TimeSpan.FromSeconds(10)
+        };
+        config.Setup(x => x.Value).Returns(configData);
+        var reportSender = new ReportSender(
+            logger.Object,
+            mapper.Object,
+            rawSender.Object,
+            serializer.Object,
+            config.Object);
+        using var cts = new CancellationTokenSource();
+
+        // Act
+        var exception = await Record.ExceptionAsync(() =>
+            reportSender.SendAsync(null!, cts.Token)
+        );
+
+        // Assert
+        exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"{nameof(ReportSender)} can't send report with cancelled token")]
+    [Trait("Category", "Unit")]
+    public async Task CannotSendReportWithCancelledToken()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ReportSender>>();
+        var mapper = new Mock<IMapper>(MockBehavior.Strict);
+        var rawSender = new Mock<IRawSender<string>>(MockBehavior.Strict);
+        var serializer = new Mock<ISerializer<TModel.HealthCheckReport, string>>(MockBehavior.Strict);
+        var config = new Mock<IOptions<ReportSenderConfiguration>>(MockBehavior.Strict);
+        var configData = new ReportSenderConfiguration
+        {
+            Url = new Uri("http://example.com"),
+            Timeout = TimeSpan.FromSeconds(10)
+        };
+        config.Setup(x => x.Value).Returns(configData);
+        var reportSender = new ReportSender(
+            logger.Object,
+            mapper.Object,
+            rawSender.Object,
+            serializer.Object,
+            config.Object);
+        using var cts = new CancellationTokenSource();
+        cts.Cancel();
+        var url = new Uri("https://example.com");
+        var checkInterval = TimeSpan.FromSeconds(20);
+        var timeout = TimeSpan.FromSeconds(60);
+        var res1 = new ResourceName("Resource1");
+        var res2 = new ResourceName("Resource2");
+        var settings = new ResourceRequestSettings(url, checkInterval, timeout);
+        var resourceHealthChecks = new List<ResourceHealthCheck>
+        {
+            new(res1, TimeSpan.FromSeconds(30),settings),
+            new(res2, TimeSpan.FromSeconds(45),settings)
+        };
+        var report = new HealthCheckReport(resourceHealthChecks);
+        
+        // Act
+        var exception = await Record.ExceptionAsync(() =>
+            reportSender.SendAsync(report, cts.Token)
+        );
+        
+        // Assert
+        exception.Should().BeOfType<OperationCanceledException>();
     }
 }
