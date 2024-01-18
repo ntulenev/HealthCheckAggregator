4dd7c630c2a26464ab4908dcc2e948ac49769875
Nikita Tulenev <n1tulenev@gmail.com>
2024-01-18 18:39:04 +0100

    Add SenderService tests

--- src/Service.Tests/SenderService.Tests.cs
+++ src/Service.Tests/SenderService.Tests.cs
@@ -25,11 +25,32 @@ public class SenderServiceTests
     {
         // Arrange
         IReportProcessor reportProcessor = null!;
-        
+
         // Act
         var exception = Record.Exception(() => { _ = new SenderService(reportProcessor); });
-        
+
         // Assert
         exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"{nameof(SenderService)} can start sender service")]
+    [Trait("Category", "Unit")]
+    public async Task SenderServiceCanStartSenderService()
+    {
+        // Arrange
+        var reportProcessor = new Mock<IReportProcessor>(MockBehavior.Strict);
+        using var stoppingToken = new CancellationTokenSource();
+        using var senderService = new SenderService(reportProcessor.Object);
+        var runCount = 0;
+        reportProcessor.Setup(x =>
+                x.ProcessAsync(It.IsAny<CancellationToken>()))
+            .Returns(() => Task.CompletedTask)
+            .Callback(() => runCount++);
+
+        // Act
+        await senderService.StartAsync(stoppingToken.Token);
+
+        // Assert
+        runCount.Should().Be(1);
     }
 }
