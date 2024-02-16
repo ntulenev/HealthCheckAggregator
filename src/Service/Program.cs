996ff8fc8c5b52081f16a308173d997d4d3d12d7
Nikita Tulenev <n1tulenev@gmail.com>
2024-02-16 20:07:32 +0100

    Add ServiceProvider validation

--- src/Service/Program.cs
+++ src/Service/Program.cs
@@ -9,6 +9,11 @@ builder.Services.RegisterState(builder.Configuration);
 builder.Services.RegisterObserver();
 builder.Services.RegisterHttpClient();
 builder.Services.RegisterSender(builder.Configuration);
+builder.Host.UseDefaultServiceProvider(x =>
+{
+    x.ValidateOnBuild = true;
+    x.ValidateScopes = true;
+});
 builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
     loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
 var app = builder.Build();
@@ -16,4 +21,3 @@ app.UseHealthChecks("/hc");
 app.UseHttpsRedirection();
 
 await app.RunAsync();
-
