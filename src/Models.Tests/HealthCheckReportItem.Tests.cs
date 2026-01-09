aa92b9a0c4bd22b80ed837660a4264be54d3bd79
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-09 09:28:50 +0100

    Updates HealthCheckReportItem tests
    Refactors the HealthCheckReportItemTests class to use namespace declarations instead of namespace blocks for improved code readability.

--- src/Models.Tests/HealthCheckReportItem.Tests.cs
+++ src/Models.Tests/HealthCheckReportItem.Tests.cs
@@ -1,53 +1,52 @@
-﻿using FluentAssertions;
+using FluentAssertions;
 
-namespace Models.Tests
+namespace Models.Tests;
+
+public class HealthCheckReportItemTests
 {
-    public class HealthCheckReportItemTests
+    [Fact(DisplayName = $"{nameof(HealthCheckReportItem)} can be created with valid params")]
+    [Trait("Category", "Unit")]
+    public void ConstructorInitializesResourceNameAndStatusProperties()
     {
-        [Fact(DisplayName = $"{nameof(HealthCheckReportItem)} can be created with valid params")]
-        [Trait("Category", "Unit")]
-        public void ConstructorInitializesResourceNameAndStatusProperties()
-        {
-            // Arrange
-            var resourceName = new ResourceName("TestResource");
-            var status = ResourceStatus.Healthy;
+        // Arrange
+        var resourceName = new ResourceName("TestResource");
+        var status = ResourceStatus.Healthy;
 
-            // Act
-            var reportItem = new HealthCheckReportItem(resourceName, status);
+        // Act
+        var reportItem = new HealthCheckReportItem(resourceName, status);
 
-            // Assert
-            reportItem.ResourceName.Should().Be(resourceName);
-            reportItem.Status.Should().Be(status);
-        }
+        // Assert
+        reportItem.ResourceName.Should().Be(resourceName);
+        reportItem.Status.Should().Be(status);
+    }
 
-        [Fact(DisplayName = $"{nameof(HealthCheckReportItem)} throws ArgumentNullException for null ResourceName")]
-        [Trait("Category", "Unit")]
-        public void ConstructorThrowsArgumentNullExceptionForNullResourceName()
-        {
-            // Arrange
-            ResourceName resourceName = null!;
-            var status = ResourceStatus.Healthy;
+    [Fact(DisplayName = $"{nameof(HealthCheckReportItem)} throws ArgumentNullException for null ResourceName")]
+    [Trait("Category", "Unit")]
+    public void ConstructorThrowsArgumentNullExceptionForNullResourceName()
+    {
+        // Arrange
+        ResourceName resourceName = null!;
+        var status = ResourceStatus.Healthy;
 
-            // Act
-            Action act = () => _ = new HealthCheckReportItem(resourceName, status);
+        // Act
+        Action act = () => _ = new HealthCheckReportItem(resourceName, status);
 
-            // Assert
-            act.Should().Throw<ArgumentNullException>();
-        }
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
 
-        [Fact(DisplayName = $"{nameof(HealthCheckReportItem)} throws ArgumentException for invalid Status")]
-        [Trait("Category", "Unit")]
-        public void ConstructorThrowsArgumentExceptionForInvalidStatus()
-        {
-            // Arrange
-            var resourceName = new ResourceName("TestResource");
-            var invalidStatus = (ResourceStatus)int.MaxValue;
+    [Fact(DisplayName = $"{nameof(HealthCheckReportItem)} throws ArgumentException for invalid Status")]
+    [Trait("Category", "Unit")]
+    public void ConstructorThrowsArgumentExceptionForInvalidStatus()
+    {
+        // Arrange
+        var resourceName = new ResourceName("TestResource");
+        var invalidStatus = (ResourceStatus)int.MaxValue;
 
-            // Act
-            Action act = () => _ =new HealthCheckReportItem(resourceName, invalidStatus);
+        // Act
+        Action act = () => _ = new HealthCheckReportItem(resourceName, invalidStatus);
 
-            // Assert
-            act.Should().Throw<ArgumentException>();
-        }
+        // Assert
+        act.Should().Throw<ArgumentException>();
     }
 }
