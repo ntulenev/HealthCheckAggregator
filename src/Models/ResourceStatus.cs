cf77f50e2858806fa0218d4c16c7acbcf1a14767
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-09 09:28:12 +0100

    Adds descriptions to resource status enum
    Adds summary documentation to the resource status enum,
    describing each state of the resource.

--- src/Models/ResourceStatus.cs
+++ src/Models/ResourceStatus.cs
@@ -1,10 +1,16 @@
-﻿namespace Models;
+namespace Models;
 
 /// <summary>
 /// Resource status.
 /// </summary>
 public enum ResourceStatus
 {
+    /// <summary>
+    /// Indicates that the health status is unhealthy.
+    /// </summary>
     Unhealthy = 0,
+    /// <summary>
+    /// Indicates that the component is healthy and operating as expected.
+    /// </summary>
     Healthy = 1
 }
