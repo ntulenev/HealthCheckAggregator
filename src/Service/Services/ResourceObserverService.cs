3afca94ee78baa0b72ad623ff73d1b3acba94f49
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-09 09:27:50 +0100

    Adds resource observer service
    Improves the health check mechanism by adding a background
    service that observes resources.

--- src/Service/Services/ResourceObserverService.cs
+++ src/Service/Services/ResourceObserverService.cs
@@ -1,28 +1,34 @@
-﻿using Abstractions.Logic;
+using Abstractions.Logic;
 
-namespace Service.Services
+namespace Service.Services;
+
+/// <summary>
+/// Service for observing resources health checks.
+/// </summary>
+public sealed class ResourceObserverService : BackgroundService
 {
     /// <summary>
-    /// Service for observing resources health checks.
+    /// Creates <see cref="ResourceObserverService"/>.
     /// </summary>
-    public sealed class ResourceObserverService : BackgroundService
+    /// <param name="resourceObserver">Resources observer.</param>
+    /// <exception cref="ArgumentNullException">
+    /// Throws exception is <paramref name="resourceObserver"/> is null.</exception>
+    public ResourceObserverService(IResourcesObserver resourceObserver)
     {
-        /// <summary>
-        /// Creates <see cref="ResourceObserverService"/>.
-        /// </summary>
-        /// <param name="resourceObserver">Resources observer.</param>
-        /// <exception cref="ArgumentNullException">
-        /// Throws exception is <paramref name="resourceObserver"/> is null.</exception>
-        public ResourceObserverService(IResourcesObserver resourceObserver)
-        {
-            ArgumentNullException.ThrowIfNull(resourceObserver);
-            _resourcesObserver = resourceObserver;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken) =>
-                                        await _resourcesObserver.ObserveAsync(stoppingToken)
-                                                                .ConfigureAwait(false);
-
-        private readonly IResourcesObserver _resourcesObserver;
+        ArgumentNullException.ThrowIfNull(resourceObserver);
+        _resourcesObserver = resourceObserver;
     }
+
+    /// <summary>
+    /// Executes the background service operation asynchronously and observes resource changes until the service is
+    /// stopped.
+    /// </summary>
+    /// <param name="stoppingToken">A cancellation token that is triggered when the host is performing a graceful shutdown. The operation should
+    /// monitor this token to stop execution promptly when cancellation is requested.</param>
+    /// <returns>A task that represents the asynchronous execution of the background service operation.</returns>
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken) =>
+                                    await _resourcesObserver.ObserveAsync(stoppingToken)
+                                                            .ConfigureAwait(false);
+
+    private readonly IResourcesObserver _resourcesObserver;
 }
