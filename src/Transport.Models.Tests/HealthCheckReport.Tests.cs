47c64fc193fb6109c93f31d98b953d1e910a1218
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-09 09:27:41 +0100

    Updates health check report tests
    Refactors health check report tests for clarity and consistency.
    
    Ensures successful serialization and deserialization of health check reports.

--- src/Transport.Models.Tests/HealthCheckReport.Tests.cs
+++ src/Transport.Models.Tests/HealthCheckReport.Tests.cs
@@ -1,78 +1,77 @@
-﻿using FluentAssertions;
+using FluentAssertions;
 
 using Newtonsoft.Json;
 
 using Models;
 
-namespace Transport.Models.Tests
+namespace Transport.Models.Tests;
+
+public class HealthCheckReportTests
 {
-    public class HealthCheckReportTests
+    [Fact(DisplayName = $"{nameof(HealthCheckReportTests)} could be serialized and deserialized")]
+    [Trait("Category", "Unit")]
+    public void JsonSerializationDeserializationSuccess()
     {
-        [Fact(DisplayName = $"{nameof(HealthCheckReportTests)} could be serialized and deserialized")]
-        [Trait("Category", "Unit")]
-        public void JsonSerializationDeserializationSuccess()
+        // Arrange
+        var created = DateTimeOffset.UtcNow;
+        var reportItems = new List<HealthCheckReportItem>
         {
-            // Arrange
-            var created = DateTimeOffset.UtcNow;
-            var reportItems = new List<HealthCheckReportItem>
+            new()
             {
-                new()
-                {
-                    ResourceName = "Resource1",
-                    Status = ResourceStatus.Healthy
-                },
-                new()
-                {
-                    ResourceName = "Resource2",
-                    Status = ResourceStatus.Unhealthy
-                }
-            };
-
-            var report = new HealthCheckReport
+                ResourceName = "Resource1",
+                Status = ResourceStatus.Healthy
+            },
+            new()
             {
-                Created = created,
-                ReportItems = reportItems
-            };
+                ResourceName = "Resource2",
+                Status = ResourceStatus.Unhealthy
+            }
+        };
 
-            // Act
-            var json = JsonConvert.SerializeObject(report);
-            var deserializedReport = JsonConvert.DeserializeObject<HealthCheckReport>(json);
-
-            // Assert
-            deserializedReport.Should().NotBeNull();
-            deserializedReport!.Created.Should().Be(created);
-            deserializedReport.ReportItems.Should().BeEquivalentTo(reportItems);
-        }
-
-        [Fact(DisplayName = $"{nameof(HealthCheckReportTests)} can be deserialized from the target json")]
-        [Trait("Category", "Unit")]
-        public void JsonDeserialization_Success()
+        var report = new HealthCheckReport
         {
-            // Arrange
-            var created = DateTimeOffset.UtcNow;
-            var reportItems = new List<HealthCheckReportItem>
-            {
-                new() {
-                    ResourceName = "Resource1",
-                    Status = ResourceStatus.Healthy
-                },
-                new() {
-                    ResourceName = "Resource2",
-                    Status = ResourceStatus.Unhealthy
-                }
-            };
+            Created = created,
+            ReportItems = reportItems
+        };
 
-            var json = $"{{\"timestamp\":\"{created:o}\",\"resources\":" +
-                $"[{{\"resource_name\":\"Resource1\",\"resource_status\":\"Healthy\"}}," +
-                $"{{\"resource_name\":\"Resource2\",\"resource_status\":\"Unhealthy\"}}]}}";
+        // Act
+        var json = JsonConvert.SerializeObject(report);
+        var deserializedReport = JsonConvert.DeserializeObject<HealthCheckReport>(json);
 
-            // Act
-            var deserializedReport = JsonConvert.DeserializeObject<HealthCheckReport>(json);
+        // Assert
+        deserializedReport.Should().NotBeNull();
+        deserializedReport!.Created.Should().Be(created);
+        deserializedReport.ReportItems.Should().BeEquivalentTo(reportItems);
+    }
 
-            // Assert
-            deserializedReport.Should().NotBeNull();
-            deserializedReport!.Created.Should().Be(created);
-            deserializedReport.ReportItems.Should().BeEquivalentTo(reportItems);
-        }
+    [Fact(DisplayName = $"{nameof(HealthCheckReportTests)} can be deserialized from the target json")]
+    [Trait("Category", "Unit")]
+    public void JsonDeserializationSuccess()
+    {
+        // Arrange
+        var created = DateTimeOffset.UtcNow;
+        var reportItems = new List<HealthCheckReportItem>
+        {
+            new() {
+                ResourceName = "Resource1",
+                Status = ResourceStatus.Healthy
+            },
+            new() {
+                ResourceName = "Resource2",
+                Status = ResourceStatus.Unhealthy
+            }
+        };
+
+        var json = $"{{\"timestamp\":\"{created:o}\",\"resources\":" +
+            $"[{{\"resource_name\":\"Resource1\",\"resource_status\":\"Healthy\"}}," +
+            $"{{\"resource_name\":\"Resource2\",\"resource_status\":\"Unhealthy\"}}]}}";
+
+        // Act
+        var deserializedReport = JsonConvert.DeserializeObject<HealthCheckReport>(json);
+
+        // Assert
+        deserializedReport.Should().NotBeNull();
+        deserializedReport!.Created.Should().Be(created);
+        deserializedReport.ReportItems.Should().BeEquivalentTo(reportItems);
     }
 }
