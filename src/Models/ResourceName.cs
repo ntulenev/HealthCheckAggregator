5d2046cf9992bf88cc5b3abe9b84d15ab0e83657
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-09 09:28:07 +0100

    Improves ResourceName class.
    Enhances the ResourceName class by overriding the Equals, GetHashCode, and ToString methods.
    
    This improves the usability of the ResourceName class, especially when used in collections or when comparing instances.
    
    The GetHashCode method is implemented to allow using ResourceName instances as keys in hash-based collections.
    
    The Equals method is implemented to correctly check equality of ResourceName instances.
    
    The ToString method is implemented to return the underlying value of the ResourceName instance.

--- src/Models/ResourceName.cs
+++ src/Models/ResourceName.cs
@@ -1,4 +1,4 @@
-﻿namespace Models;
+namespace Models;
 
 /// <summary>
 /// Name for a resource.
@@ -31,11 +31,31 @@ public sealed class ResourceName
         Value = name;
     }
 
+    /// <summary>
+    /// Determines whether the specified object is equal to the current ResourceName instance.
+    /// </summary>
+    /// <param name="obj">The object to compare with the current ResourceName instance.</param>
+    /// <returns>true if the specified object is a ResourceName and its value is equal to the value of the current instance;
+    /// otherwise, false.</returns>
     public override bool Equals(object? obj)
         => obj is ResourceName name &&
            Value == name.Value;
 
+    /// <summary>
+    /// Serves as the default hash function.
+    /// </summary>
+    /// <remarks>Use this method when inserting instances of this type into hash-based collections such as
+    /// <see cref="Dictionary{TKey, TValue}"/> or <see
+    /// cref="HashSet{T}"/>. The hash code is based on the value of the underlying
+    /// <c>Value</c> property.</remarks>
+    /// <returns>A 32-bit signed integer hash code for the current object.</returns>
+#pragma warning disable CA1307 // Specify StringComparison for clarity
     public override int GetHashCode() => Value.GetHashCode();
+#pragma warning restore CA1307 // Specify StringComparison for clarity
 
+    /// <summary>
+    /// Returns the string representation of the current object.
+    /// </summary>
+    /// <returns>A string that represents the value of the current object.</returns>
     public override string ToString() => Value;
 }
